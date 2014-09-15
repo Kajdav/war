6c10ba8d3957594e562abe98c3a11c0d29c5723e
kajdav <poppa.bear.joe@gmail.com>
2014-09-15 16:54:31 -0600

    completes javascript to finish game. includes main gameplay and accounts for ties.  has some testing code included.

--- script.js
+++ script.js
@@ -33,7 +33,8 @@ $(document).ready(function() {
 		var copy = [];
 		var n = array.length; 
 		var i; 
-		while (n) { i = Math.floor(Math.random() * array.length);  
+		while (n) { 
+			i = Math.floor(Math.random() * array.length);  
 			if (i in array) { 
 		 		copy.push(array[i]); 
 		 		delete array[i]; 
@@ -46,18 +47,89 @@ $(document).ready(function() {
 	var cards_player_1 = [];
 	var cards_player_2 = [];
 	//divide out the cards into the two arrays
-	
+
+	var deal = function(array) {
+		cards_player_1 = array.slice(0, 26);
+		cards_player_2 = array.slice(26);
+	}
+	deal(shuffle(deck));
 	
 	//create a function (algorithm) called "war" that takes two cards as parameters, compares them and returns a winner. A tie should return false.
-	function war() {
+	var war = function (card1, card2) {
+		if (card1 > card2) {
+			return 1;
+		}
+		else if (card1 < card2) {
+			return 2;
+		}
+		else if (card1 === card2) {
+			return false;
+		}
 	}
-	
-	
-	//create a play function
-		//compare the cards
-		//give the winner both cards (at end of deck)
+	var tie = function() {
+		var outcomeTie = war(cards_player_1[3].number, cards_player_2[3].number);
+		if (outcomeTie === 1) {
+			for(var i = 0; i < 4; i++){
+				cards_player_1.push(cards_player_1[i]);
+				cards_player_1.push(cards_player_2[i]);
+			}
+			cards_player_1.splice(0, 4);
+			cards_player_2.splice(0, 4);
+		}
+		else if (outcomeTie === 2) {
+			for(var i = 0; i < 4; i++){
+				cards_player_2.push(cards_player_1[i]);
+				cards_player_2.push(cards_player_2[i]);
+			}
+			cards_player_1.splice(0, 4);
+			cards_player_2.splice(0, 4);
+		}
+		else if (outcomeTie === false) {
+			alert("42");
+			cards_player_1.push(cards_player_1[0]);
+			cards_player_2.push(cards_player_2[0]);
+			cards_player_1.splice(0, 1);
+			cards_player_2.splice(0, 1);
+		}
+	}
+	// console.log(cards_player_1);
+	// var showHand = function () {
+	//     var firstHand = [];
+	//     var secondHand = [];
+	//     var displayHand = function (cards, hand) {
+	//         for (var i = 0; i < cards.length; i++) {
+	//             hand[i] = cards[i].number + " of " + cards[i].suit;
+	//         }
+	//     }
+	//     displayHand(cards_player_1, firstHand);
+	//     displayHand(cards_player_2, secondHand);
+	//     console.log("Player One's Hand: " + firstHand)
+	//     console.log("Player Two's Hand: " + secondHand)
+	// }
+	//The above, commented code is for testing.  It shows both players' hands in the console every 10 plays.
+
+
+	var counter = 0;
 	function play() {
-		
+		// if (counter % 10 ===0) {
+		// 	showHand();
+		// }   <---- This corresponds with the commented out code above.
+		counter++;
+		var outcome = war(cards_player_1[0].number, cards_player_2[0].number);
+		counter++;
+		if (outcome === 1) {
+			cards_player_1.push(cards_player_1[0], cards_player_2[0]);
+			cards_player_1.splice(0, 1);
+			cards_player_2.splice(0, 1);
+		}
+		else if (outcome === 2) {
+			cards_player_2.push(cards_player_1[0], cards_player_2[0]);
+			cards_player_1.splice(0, 1);
+			cards_player_2.splice(0, 1);
+		}
+		else if (outcome === false) {
+			tie();
+		}
 		//this function (defined below) will continue to the next turn
 		advance();
 	}
